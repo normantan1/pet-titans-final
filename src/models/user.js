1e7f5b93acd316ee3b6b0da89b5cb2482bd18ff6
ong-wei-hong <weihong.ong@outlook.com>
2023-06-18 10:06:45 +0800

    changes

--- src/models/user.js
+++ src/models/user.js
@@ -4,8 +4,9 @@ import api from "../api"
 
 export const createUser = async ({ name, email, password }) => {
 	try {
-		const res = api.users.createUser({ name, email, password });
-		console.log(res.error);
+		const res = await api.users.createUser({ name, email, password });
+		// TODO check if environ is dev, then print
+		console.log(res.devMessage);
 		// TODO flash res.message to app screen
 	} catch(err) {
 		throw err;
