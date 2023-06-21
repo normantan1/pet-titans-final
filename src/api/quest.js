e76709de768d8d37f43d9d93de3b7705cb524790
revdrag <94689523+revdrag@users.noreply.github.com>
2023-06-21 23:14:12 +0800

    Side Quest Feature Update

--- src/api/quest.js
+++ src/api/quest.js
@@ -2,51 +2,71 @@ const quest = [
     {
         id: 1,
         title : 'walk 10000 steps',
+        exp: 45,
+        credits: 15,
         numberOfPeopleCompleted : 0
     }, 
     {
         id: 2,
         title : 'lunch with tablemates',
+        exp: 40,
+        credits: 12,
         numberOfPeopleCompleted : 0
     },
     {
         id: 3,
         title : 'drink 8 cups of water',
+        exp: 25,
+        credits: 10,
         numberOfPeopleCompleted : 0
     }, 
     {
         id: 4,
         title : 'stand up and stretch your back',
+        exp: 13,
+        credits: 10,
         numberOfPeopleCompleted : 0
     }, 
     {
         id: 5,
         title : 'look away from the screen for 10 minute',
+        exp: 15,
+        credits: 6,
         numberOfPeopleCompleted : 0
     }, 
 	{
 		id: 6,
 		title: 'remember to take a breather! stand up and stretch!',
+        exp: 17,
+        credits: 8,
 		numberOfPeopleCompleted: 0,
 	},
 	{
 		id: 7,
 		title: 'have you taken a sip of water? drink a cup of water!',
+        exp: 5,
+        credits: 2,
 		numberOfPeopleCompleted: 0,
 	},
 	{
 		id: 8,
 		title: 'have you smiled today? smile to your neighbour now!',
+        exp: 10,
+        credits: 2,
 		numberOfPeopleCompleted: 0,
 	},
 	{
 		id: 9,
 		title: 'complete one focus session today',
+        exp: 10,
+        credits: 2,
 		numberOfPeopleCompleted: 0,
 	},
 	{
 		id: 10,
 		title: "lets declutter our workspace, it's getting messy",
+        exp: 50,
+        credits: 15,
 		numberOfPeopleCOmpleted: 0,
 	}
 ]
